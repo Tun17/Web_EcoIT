466c30838f8899fd9c41f5e0d25424f42b5bcb18
DESKTOP-EBP024J\ASUS <tunnph25693@fpt.edu.vn>
2024-03-27 14:18:19 +0700

    Update

--- src/app/modules/navigator/nav-list/nav-list.component.ts
+++ src/app/modules/navigator/nav-list/nav-list.component.ts
@@ -1,41 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import {Navigator} from "../../../core/model/navigator/navigator";
-import {About} from "../../../core/model/about/about";
-import {AboutService} from "../../../services/about/about.service";
-import {NavigationService} from "../../../services/navigation/navigation.service";
+import { Navigator } from '../../../core/model/navigator/navigator';
+import { About } from '../../../core/model/about/about';
+import { AboutService } from '../../../services/about/about.service';
+import { NavigationService } from '../../../services/navigation/navigation.service';
 
 @Component({
   selector: 'app-nav-list',
   templateUrl: './nav-list.component.html',
-  styleUrls: ['./nav-list.component.css']
+  styleUrls: ['./nav-list.component.css'],
 })
 export class NavListComponent implements OnInit {
-
   username: any;
 
   navParent: Navigator[] = [];
 
   about: About = new About();
 
-  constructor(private navService: NavigationService,
-              private aboutService: AboutService) { }
+  constructor(
+    private navService: NavigationService,
+    private aboutService: AboutService
+  ) {}
 
   ngOnInit(): void {
     this.getAllNav();
     this.getAbout();
   }
 
-  getAllNav(){
-    this.navService.getNavList().subscribe(data => {
+  getAllNav() {
+    this.navService.getNavList().subscribe((data) => {
       this.navParent = data;
     });
   }
 
-  getAbout(){
-    this.aboutService.getInfo().subscribe(data =>{
-      if (data != null){
+  getAbout() {
+    this.aboutService.getInfo().subscribe((data) => {
+      if (data != null) {
         this.about = data;
       }
-    })
+    });
   }
 }
